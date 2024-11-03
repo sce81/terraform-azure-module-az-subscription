f6296b73438cc91a5d6e9ef92110481093491d09
Simon Elliott <simon.elliott@hashicorp.com>
2024-11-03 19:14:43 +0000

    develop - validating

--- data.tf
+++ data.tf
@@ -1,4 +1,7 @@
 data "azurerm_billing_enrollment_account_scope" "main" {
   billing_account_name    = var.billing_account_name
-  enrollment_account_name = var.enrollment_account_name
+  enrollment_account_name = "existing" //var.enrollment_account_name
+}
+
+data "azurerm_subscription" "current" {
 }
--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 resource "azurerm_subscription" "main" {
+ alias =  var.subscription_name
   subscription_name = var.subscription_name
-  billing_scope_id  = data.azurerm_billing_enrollment_account_scope.main.id
+   subscription_id   = data.azurerm_subscription.current.subscription_id
+  //billing_scope_id  = data.azurerm_billing_enrollment_account_scope.main.id
 }
